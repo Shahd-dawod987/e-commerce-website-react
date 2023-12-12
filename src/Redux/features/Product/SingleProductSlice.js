178a6c7f59983edf59cf21d7a64dbc7a2881b5a3
Shahd Dawod <71906350+shahd123-png@users.noreply.github.com>
2023-12-12 13:38:42 +0200

    Update SingleProductSlice.js

--- src/Redux/features/Product/SingleProductSlice.js
+++ src/Redux/features/Product/SingleProductSlice.js
@@ -1,13 +1,32 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-
 export const fetchProductDetails = createAsyncThunk(
   "productDetails/fetchProductDetails",
-  async (productId) => {
-    const response = await axios.get(`https://fakestoreapi.com/products/${productId}`);
-    return response.data;
+  async (productId, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        `https://fakestoreapi.com/products/${productId}`
+      );
+
+      if (response == null) {
+        return rejectWithValue("Response is null or undefined");
+      }
+      if (response.status >= 200 && response.status < 300) {
+        return response.data;
+      } else 
+      {
+        return rejectWithValue(
+          `Failed to fetch product details. Status: ${response.status}`
+        );
+      }
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data||error.message || "Failed to fetch product details"
+      );
+    }
   }
 );
+
 
 const singleProductReducer = createSlice({
   name: "singleProduct",
