fce6285f2e048d902b773d884347162b8f3eb207
Shahd Dawod <71906350+shahd123-png@users.noreply.github.com>
2023-12-12 13:38:01 +0200

    Update CartSlice.js

--- src/Redux/features/Cart/CartSlice.js
+++ src/Redux/features/Cart/CartSlice.js
@@ -2,60 +2,66 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const addToCart = createAsyncThunk("cart/addToCart", async (product, { getState }) => {
-  const currentDate = new Date().toISOString().split("T")[0];
+  try {
+    const currentDate = new Date().toISOString().split("T")[0];
+    const state = getState();
+    const existingProduct = state.cart.cartItems.find(item => item.productId === product.productId);
 
-  const state = getState();
-  const existingProduct = state.cart.cartItems.find(item => item.productId === product.productId);
+    let response;
+    if (existingProduct) {
+      const updatedProducts = state.cart.cartItems.map(item =>
+        item.productId === product.productId
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      );
 
-  if (existingProduct) {
-    const updatedProducts = state.cart.cartItems.map(item =>
-      item.productId === product.productId
-        ? { ...item, quantity: item.quantity + 1 }
-        : item
-    );
+      response = await axios.put(`https://fakestoreapi.com/carts/3`, {
+        userId: 2,
+        date: currentDate,
+        products: updatedProducts,
+      });
+    } else {
+      response = await axios.put(`https://fakestoreapi.com/carts/3`, {
+        userId: 2,
+        date: currentDate,
+        products: [...state.cart.cartItems, { ...product, quantity: 1 }],
+      });
+    }
 
-    const response = await axios.put(`https://fakestoreapi.com/carts/3`, {
-      userId: 2,
-      date: currentDate,
-      products: updatedProducts,
-    });
-
-    return response?.data;
-  } else {
-    const response = await axios.put(`https://fakestoreapi.com/carts/3`, {
-      userId: 2,
-      date: currentDate,
-      products: [...state.cart.cartItems, { ...product, quantity: 1 }],
-    });
-
-    return response?.data;
+    return response?.data || {};
+  } catch (error) {
+    console.error( error.response.data||error.message || "Failed to fetch product details");
+    throw error;
   }
 });
 
 export const updateQuantity = createAsyncThunk(
   "cart/updateQuantity",
   async ({ productId, quantity }, { getState }) => {
-    const currentDate = new Date().toISOString().split("T")[0];
+    try {
+      const currentDate = new Date().toISOString().split("T")[0];
+      const state = getState();
+      const updatedItems = state.cart.cartItems.map((item) =>
+        item.productId === productId ? { ...item, quantity } : item
+      );
 
-    const state = getState();
-    const updatedItems = state.cart.cartItems.map((item) =>
-      item.productId === productId ? { ...item, quantity } : item
-    );
+      const response = await axios.put(`https://fakestoreapi.com/carts/3`, {
+        userId: 2,
+        date: currentDate,
+        products: updatedItems,
+      });
 
-    const response = await axios.put(`https://fakestoreapi.com/carts/3`, {
-      userId: 2,
-      date: currentDate,
-      products: updatedItems,
-    });
-
-    return { ...response?.data, totalPrice: calculateTotalPrice(updatedItems) };
+      return { ...response?.data, totalPrice: calculateTotalPrice(updatedItems) } || {};
+    } catch (error) {
+      console.error( error.response.data||error.message || "Failed to fetch product details");
+      throw error;
+    }
   }
 );
 
 const calculateTotalPrice = (items) => {
   return items.reduce((total, item) => total + item.price * item.quantity, 0);
 };
-
 
 const cartReducer = createSlice({
   name: "cart",
@@ -78,8 +84,8 @@ const cartReducer = createSlice({
       })
       .addCase(addToCart.fulfilled, (state, action) => {
         state.status = "succeeded";
-        console.log(action.payload); 
-        state.cartItems = action.payload.products;  
+        console.log(action.payload);
+        state.cartItems = action.payload.products || [];
       })
       .addCase(addToCart.rejected, (state, action) => {
         state.status = "failed";
@@ -88,13 +94,13 @@ const cartReducer = createSlice({
 
       .addCase(updateQuantity.fulfilled, (state, action) => {
         state.status = "succeeded";
-        state.cartItems = action.payload.products;
-        state.totalPrice = action.payload.totalPrice;
+        state.cartItems = action.payload.products || [];
+        state.totalPrice = action.payload.totalPrice || 0;
       });
   },
 });
 
-export {addToCart};
+export { addToCart };
 export const { removeFromCart } = cartReducer.actions;
 
 export default cartReducer.reducer;
